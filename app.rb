d5cdbc3be0840d80c8b178a55ef4b4e604c1a3a2
Tony <anthonyshikanga@gmail.com>
2017-07-21 14:15:50 +0300

    *included route for index page in app.rb file

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require("bundler/setup")
  Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
  also_reload("lib/**/*.rb")
 
+ get('/') do
+   erb(:index)
+ end
+
  #used bundler to require all of our gems for us rather than listing them individually in each file
  #used Dir class as a file loading shortcut to navigate to the directories with files that need to be required
  #__FILE__ refers to current file and File.dirname(__FILE__) will give current directory
