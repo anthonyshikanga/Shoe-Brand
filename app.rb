2f9463af3b1d23f0515acf3d14b7f684992ca9a5
Tony <anthonyshikanga@gmail.com>
2017-07-22 14:07:09 +0300

    created stores.erb file

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ require("bundler/setup")
    erb(:index)
  end
 
+ get('/stores') do
+   erb(:stores)
+ end
+
  #used bundler to require all of our gems for us rather than listing them individually in each file
  #used Dir class as a file loading shortcut to navigate to the directories with files that need to be required
  #__FILE__ refers to current file and File.dirname(__FILE__) will give current directory
