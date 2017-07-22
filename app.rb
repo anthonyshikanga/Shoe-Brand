ec17d9e894525b5f4c316e1c63e06081fd0788ae
Tony <anthonyshikanga@gmail.com>
2017-07-22 16:18:11 +0300

    modified files

--- app.rb
+++ app.rb
@@ -7,9 +7,34 @@ require("bundler/setup")
    erb(:index)
  end
 
+ post('/stores') do
+   name = params.fetch("name")
+   store = Store.new(:name =>name)
+   store.save()
+   @stores = Store.all()
+   redirect to ('/')
+ end
+
  get('/stores') do
+   @stores = Store.all()
    erb(:stores)
  end
+
+ get("/stores/:id") do
+  @store = Store.find(params.fetch("id").to_i())
+  erb(:store)
+end
+
+ get('/brands') do
+   erb(:brands)
+ end
+
+ get('/stores/new') do
+   erb(:store_form)
+ end
+
+
+
 
  #used bundler to require all of our gems for us rather than listing them individually in each file
  #used Dir class as a file loading shortcut to navigate to the directories with files that need to be required
--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,6 @@
 class Store < ActiveRecord::Base
   has_many(:shoes)
+  validates(:name, :presence => true)
 end
 
 #Store class inherits from ActiveRecord and added relationship
