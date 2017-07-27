60c2f5625c116c4e409c6cbd4dde02b9ffe50ac0
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-27 16:48:25 +0300

    modified files

--- app.rb
+++ app.rb
@@ -66,6 +66,31 @@ delete('/stores/:id/delete') do
   erb(:stores)
 end
 
+get '/brands/:id' do
+  @brand = Shoe.find(params.fetch("id").to_i())
+  erb(:brand)
+end
+
+get '/brands/:id/edit' do
+@brand = Shoe.find(params.fetch("id").to_i)
+erb(:edit_brand)
+end
+
+patch '/brands/:id' do
+  @brand = Shoe.find(params.fetch("id").to_i)
+  @brand_name = params.fetch('name')
+  @brand.name = @brand_name
+  @brand.update(name: @brand_name)
+  redirect '/'
+end
+
+delete('/brands/:id/delete') do
+  @brands = Shoe.all
+  @brand = Shoe.find(params.fetch("id").to_i)
+  @brand.destroy
+  erb(:brands)
+end
+
  #used bundler to require all of our gems for us rather than listing them individually in each file
  #used Dir class as a file loading shortcut to navigate to the directories with files that need to be required
  #__FILE__ refers to current file and File.dirname(__FILE__) will give current directory
